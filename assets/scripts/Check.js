6ea71e7114baef944e388194bca427ba0965970d
Ekaterina Bulyga <katja1232545@gmail.com>
2020-04-06 17:08:41 +0400

    Update Check.js

---
+++ assets/scripts/Check.js
@@ -0,0 +1,4 @@
+if (document.cookie == undefined) {
+    document.cookie = "name=user1; max-age=3600";
+    alert('Сайт находится в процессе разработки. Некоторые данные могут быть не доработаны.');
+}
