abdc4f9965d8939a76dacb95fcf613a1436169c9
Ekaterina Bulyga <katja1232545@gmail.com>
2020-04-23 21:25:47 +0400

    Update comment.php

--- Pages/comment.php
+++ Pages/comment.php
@@ -8,9 +8,7 @@
   $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
   $mysqli->query("INSERT INTO `comments` (`name`, `page_id`, `text_comment`) VALUES ('$name', '$page_id', '$text_comment')");// Добавляем комментарий в таблицу
   header("Location: ".$_SERVER["HTTP_REFERER"]);// Делаем реридект обратно
-?>
 
-<?php
   $page_id = 150;// Уникальный идентификатор страницы (статьи или поста)
   $mysqli = new mysqli("localhost", "root", "", "db");// Подключается к базе данных
   $result_set = $mysqli->query("SELECT * FROM `comments` WHERE `page_id`='$page_id'"); //Вытаскиваем все комментарии для данной страницы
